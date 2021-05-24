9f710ea1189eaf42378ad11828d23fa700a6f0b6
Semyon-Ivakaev <stixonforpython@gmail.com>
2021-05-24 15:39:16 +0300

    update Tasks

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,17 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        val copy = sadArray.toMutableList()
+        var n = 1
+        while (n < copy.size - 1) {
+            if (n != 0 && copy[n] > copy[n + 1] + copy[n - 1]) {
+                copy.removeAt(n)
+                n--
+            } else {
+                n++
+            }
+        }
+
+        return copy.toIntArray()
     }
 }
--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,6 +4,66 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val arr = mutableListOf<String>()
+
+        for (el in inputString.indices) {
+            when (inputString[el]) {
+                '<' -> {
+                    var str = ""
+                    for (i in el + 1..inputString.length) {
+                        if (inputString[i] == '>') {
+                            arr.add(str)
+                            str += inputString[i]
+                            for (j in i + 1 until inputString.length) {
+                                if (inputString[j] == '<') {
+                                    break
+                                } else if (inputString[j] == '>') {
+                                    arr.add(str)
+                                    str += inputString[j]
+                                } else str += inputString[j]
+                            }
+                            break
+                        } else str += inputString[i]
+                    }
+                }
+                '[' -> {
+                    var str = ""
+                    for (i in el + 1..inputString.length) {
+                        if (inputString[i] == ']') {
+                            arr.add(str)
+                            str += inputString[i]
+                            for (j in i + 1 until inputString.length) {
+                                if (inputString[j] == ']') {
+                                    arr.add(str)
+                                    str += inputString[j]
+                                } else if (inputString[j] == '[') {
+                                    break
+                                } else str += inputString[j]
+                            }
+                            break
+                        } else str += inputString[i]
+                    }
+                }
+                '(' -> {
+                    var str = ""
+                    for (i in el + 1..inputString.length) {
+                        if (inputString[i] == ')') {
+                            arr.add(str)
+                            str += inputString[i]
+                            for (j in i + 1 until inputString.length) {
+                                if (inputString[j] == ')') {
+                                    arr.add(str)
+                                    str += inputString[j]
+                                } else if (inputString[j] == '(') {
+                                    break
+                                } else str += inputString[j]
+                            }
+                            break
+                        } else str += inputString[i]
+                    }
+                }
+            }
+        }
+        return arr.toTypedArray()
     }
 }
